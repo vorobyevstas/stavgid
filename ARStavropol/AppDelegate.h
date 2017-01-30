022f27d476db9545beda4dbc7255f8764141fd4d
apple <ios.stavropol@gmail.com>
2017-01-30 23:34:01 +0300

    Initial Commit

---
+++ ARStavropol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARStavropol
+//
+//  Created by apple on 30.01.17.
+//  Copyright © 2017 Vorobyev Stanislav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARStavropol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARStavropol
+//
+//  Created by apple on 30.01.17.
+//  Copyright © 2017 Vorobyev Stanislav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
